849f10326a570ee6f31158e7d8ea5168e58513e2
Alexandr Filshyn <afilshin@dev.smk>
2018-12-17 19:05:59 +0200

    6 3-40

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import styled from 'styled-components';
 
 const StyledPage = styled.div`
-  
+  border: 1px solid red;
 `;
 
 class Page extends Component {
   render() {
     return (
-      <>
+      <StyledPage>
         <Header/>
         {this.props.children}
-      </>
+      </StyledPage>
     );
   }
 }
